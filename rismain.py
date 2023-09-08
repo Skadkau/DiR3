a44241dfb16f5db17b5f2cc424121c1ff7f0e9a7
skadkau <dashas.skachkova@mail.ru>
2023-09-08 15:14:42 +0300

    shag 1

--- rismain.py
+++ rismain.py
@@ -1,18 +1,3 @@
 import os
 from matplotlib import pyplot as plt   
-#ploting our canvas
-#plt.plot([-20,-10,0, 10, 20],[20,10,0, -10, -20])
-plt.plot([0, 35.2],[28, 0])
-plt.plot([19, 19],[0, 20])
-plt.plot([0, 25],[17, 17])
-plt.plot([0, 21.25],[17, 0])
-plt.arrow(0, 0, 7.7, 11)
-plt.text(5, 25, 'y <= -0.8x + 28.2')
-plt.text(20, 5, 'x <= 19')
-plt.text(3, 19, 'y <= 17')
-plt.text(5, 4, 'vector')
-plt.grid()
-plt.title('ПЗ № 1')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()   
+#ploting our canvas   
