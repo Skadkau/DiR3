ac0416d5f017a10009bcb69055ca0004480d9e7d
skadkau <dashas.skachkova@mail.ru>
2023-09-08 15:15:08 +0300

    shag 2

--- rismain.py
+++ rismain.py
@@ -1,3 +1,8 @@
 import os
 from matplotlib import pyplot as plt   
 #ploting our canvas   
+#plt.plot([-20,-10,0, 10, 20],[20,10,0, -10, -20])
+plt.plot([0, 35.2],[28, 0])
+plt.plot([19, 19],[0, 20])
+plt.plot([0, 25],[17, 17])
+plt.plot([0, 21.25],[17, 0])
